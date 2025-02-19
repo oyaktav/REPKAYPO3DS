3ccb7ae2d48703a83294e4f40772e90500a0118d
oyaktav <166817655+oyaktav@users.noreply.github.com>
2025-02-19 07:46:14 -0300

    KAYO 3DS 12/02/25

---
+++ TEMPO ESTIMADO DE VIAGEM.py
@@ -0,0 +1,6 @@
+ dist = float (input("Informe a distancia do seu destino (em km):"))
+km = float (input("Informe a velociadade media em (km/h):"))
+
+temp= dist/km
+
+print ("O tempo aproximado que voce levara para chegar ao seu destino é de :",temp,"horas.")
